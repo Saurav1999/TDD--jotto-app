4bb3f5d79f415625d58e6a1fc5686d826e831337
Saurav1999 <singhsaurav1999@gmail.com>
2021-03-15 22:11:56 +0530

    Complete Input component render tests for success=false

--- src/Input.test.js
+++ src/Input.test.js
@@ -14,14 +14,24 @@ setup()
 describe('render', () => {
   
     describe('word has not been guessed', () => {
+        let wrapper;
+        beforeEach(() =>{
+            const initialState = {success:false};
+            wrapper = setup(initialState)
+
+        })
       test('renders the component without error', () => {
-          
-      })
+          const component = findByTestAttr(wrapper, 'component-input');
+          expect(component.length).toBe(1);
+      }) 
       test('render input box', () => {
-          
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+        expect(inputBox.length).toBe(1);
+
       })
       test('render submit button', () => {
-          
+        const submitButton = findByTestAttr(wrapper, 'submit-button');
+        expect(submitButton.length).toBe(1);
       })
       
       
